ab11c1139c5d85e2505ad391a1df3aa9faf92e92
Oleksandr Fediashov <olfedias@microsoft.com>
2023-01-17 17:05:12 +0100

    improve fixture

--- fixture-simple/index.js
+++ fixture-simple/index.js
@@ -1,9 +1,11 @@
 import { styled } from "@linaria/react";
 import React from "react";
 
+import bgColor from "./bgColor";
 import "./side-effect";
 
 const Title = styled.h1`
+  background: ${bgColor};
   color: ${process.env.FIXTURE_COLOR};
 `;
 
